efbebc86135e6a48007cab2bb85ad543748e421f
Mehmet Mevlevi <60301417+Mehmet-pro@users.noreply.github.com>
2020-02-22 23:39:18 +0300

    X O game

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetScoreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 51);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(113, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(51, 51);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(170, 49);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(51, 51);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(56, 106);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(51, 51);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(113, 106);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(51, 51);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(170, 106);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(51, 51);
+            this.button6.TabIndex = 3;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button_click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(56, 163);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(51, 51);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button_click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(113, 163);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(51, 51);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button_click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(170, 163);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(51, 51);
+            this.button9.TabIndex = 6;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button_click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 253);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "X :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 291);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "O :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 320);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Draw :";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.resetScoreToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 15;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // resetScoreToolStripMenuItem
+            // 
+            this.resetScoreToolStripMenuItem.Name = "resetScoreToolStripMenuItem";
+            this.resetScoreToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.resetScoreToolStripMenuItem.Text = "Reset Score";
+            this.resetScoreToolStripMenuItem.Click += new System.EventHandler(this.resetScoreToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.aboutToolStripMenuItem.Text = "Flip Teams";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 411);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetScoreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,391 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+        public bool player = true;
+        public bool draws = false;
+        public bool flip = true;
+        public bool flip2 = false;
+        public int turn = 0;
+        public bool drawstar = false;
+        public int p1 = 0;
+        public int p2 = 0;
+        public int draw = 0;
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+
+
+            if (player == true && b.Text == "")
+            {
+
+                b.Text = "X";
+                player = false;
+                turn++;
+
+
+            } else if (player == false && b.Text == "")
+            {
+                b.Text = "O";
+                player = true;
+                turn++;
+
+
+            }
+
+
+
+            if (button1.Text == button2.Text && button2.Text == button3.Text && button3.Text != "")
+            {
+
+                if (b.Text == "X")
+                {
+
+                    MessageBox.Show("X Wins!");
+                    if (flip == true && flip2 == false)
+                    {
+                        p1++;
+                    }else
+                    {
+                        p2++;
+                    }
+                }
+                else if (b.Text == "O")
+                {
+
+                    MessageBox.Show("O Wins!");
+                    if (flip == true && flip2 == false)
+                    {
+                        p2++;
+                    }
+                    else
+                    {
+                        p1++;
+                    }
+                }
+
+                reset_buttons();
+
+            }
+            else if (button4.Text == button5.Text && button5.Text == button6.Text && button6.Text != "")
+            {
+                if (b.Text == "X")
+                {
+                    MessageBox.Show("X Wins!");
+                    if (flip == true && flip2 == false)
+                    {
+                        p1++;
+                    }
+                    else
+                    {
+                        p2++;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("O Wins!");
+                    if (flip == true && flip2 == false)
+                    {
+                        p2++;
+                    }
+                    else
+                    {
+                        p1++;
+                    }
+                }
+                reset_buttons();
+            }
+            else if (button7.Text == button8.Text && button8.Text == button9.Text && button9.Text != "")
+            {
+                if (b.Text == "X")
+                {
+                    MessageBox.Show("X Wins!");
+                    if (flip == true && flip2 == false)
+                    {
+                        p1++;
+                    }
+                    else
+                    {
+                        p2++;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("O Wins!");
+                    if (flip == true && flip2 == false)
+                    {
+                        p2++;
+                    }
+                    else
+                    {
+                        p1++;
+                    }
+                }
+                reset_buttons();
+            }
+            else if (button1.Text == button4.Text && button4.Text == button7.Text && button7.Text != "")
+            {
+
+                if (b.Text == "X")
+                {
+                    MessageBox.Show("X Wins!");
+                    if (flip == true && flip2 == false)
+                    {
+                        p1++;
+                    }
+                    else
+                    {
+                        p2++;
+                    }
+                }
+                else if (b.Text == "O")
+                {
+                    MessageBox.Show("O Wins!");
+                    if (flip == true && flip2 == false)
+                    {
+                        p2++;
+                    }
+                    else
+                    {
+                        p1++;
+                    }
+                }
+                reset_buttons();
+
+            }
+            else if (button2.Text == button5.Text && button5.Text == button8.Text && button8.Text != "")
+            {
+
+                if (b.Text == "X")
+                {
+                    MessageBox.Show("X Wins!");
+                    if (flip == true && flip2 == false)
+                    {
+                        p1++;
+                    }
+                    else
+                    {
+                        p2++;
+                    }
+                }
+                else if (b.Text == "O")
+                {
+                    MessageBox.Show("O Wins!");
+                    if (flip == true && flip2 == false)
+                    {
+                        p2++;
+                    }
+                    else
+                    {
+                        p1++;
+                    }
+                }
+
+                reset_buttons();
+            }
+            else if (button3.Text == button6.Text && button6.Text == button9.Text && button9.Text != "")
+            {
+
+                if (b.Text == "X")
+                {
+                    MessageBox.Show("X Wins!");
+                    if (flip == true && flip2 == false)
+                    {
+                        p1++;
+                    }
+                    else
+                    {
+                        p2++;
+                    }
+                }
+                else if (b.Text == "O")
+                {
+                    MessageBox.Show("O Wins!");
+                    if (flip == true && flip2 == false)
+                    {
+                        p2++;
+                    }
+                    else
+                    {
+                        p1++;
+                    }
+                }
+
+                reset_buttons();
+
+            }
+            else if (button3.Text == button5.Text && button5.Text == button7.Text && button7.Text != "")
+            {
+                if (b.Text == "X")
+                {
+                    MessageBox.Show("X Wins!");
+                    if (flip == true && flip2 == false)
+                    {
+                        p1++;
+                    }
+                    else
+                    {
+                        p2++;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("O Wins!");
+                    if (flip == true && flip2 == false)
+                    {
+                        p2++;
+                    }
+                    else
+                    {
+                        p1++;
+                    }
+                }
+
+                reset_buttons();
+
+            }
+            else if (button1.Text == button5.Text && button5.Text == button9.Text && button9.Text != "")
+            {
+                if (b.Text == "X")
+                {
+                    MessageBox.Show("X Wins!");
+                    if (flip == true && flip2 == false)
+                    {
+                        p1++;
+                    }
+                    else
+                    {
+                        p2++;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("O Wins!");
+                    if (flip == true && flip2 == false)
+                    {
+                        p2++;
+                    }
+                    else
+                    {
+                        p1++;
+                    }
+                }
+
+                reset_buttons();
+
+            } else if (turn == 9)
+            {
+                drawstar = true;
+                draws = true;
+            }
+
+
+            if (draws == true && drawstar == true)
+            {
+                MessageBox.Show("Draw!");
+                draw++;
+                
+                reset_buttons();
+            }
+
+            if (flip2 == false)
+            {
+                label1.Text = "X :" + p1;
+                label2.Text = "O :" + p2;
+                label3.Text = "Draw :" + draw;
+
+            }
+            else if (flip2 == true)
+            {
+                label1.Text = "X :" + p2;
+                label2.Text = "O :" + p1;
+                label3.Text = "Draw :" + draw;
+
+            }
+
+
+
+        }
+
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            player = true;
+            draws = false;
+            turn = 0;
+            button1.Text = button2.Text = button3.Text = button4.Text = button5.Text = button6.Text = button7.Text = button8.Text = button9.Text = "";
+        }
+
+        private void resetScoreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            player = true;
+            turn = 0;
+            draws = false;
+            button1.Text = button2.Text = button3.Text = button4.Text = button5.Text = button6.Text = button7.Text = button8.Text = button9.Text = "";
+            p1 = 0;
+            p2 = 0;
+            draw = 0;
+            label1.Text = "X :" + p1;
+            label2.Text = "O :" + p2;
+            label3.Text = "Draw :" + draw;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void reset_buttons()
+        {
+            button1.Text = button2.Text = button3.Text = button4.Text = button5.Text = button6.Text = button7.Text = button8.Text = button9.Text = "";
+            turn = 0;
+            drawstar = false;
+            draws = false;
+            player = true;
+
+    }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            
+
+            if (flip == true)
+            {
+                label1.Text = "X :" + p2;
+                label2.Text = "O :" + p1;
+                label3.Text = "Draw :" + draw;
+                flip = false;
+                flip2 = true;
+            }
+            else
+            {
+                label1.Text = "X :" + p1;
+                label2.Text = "O :" + p2;
+                label3.Text = "Draw :" + draw;
+                flip = true;
+                flip2 = false;
+            }
+        }
+
+     
+    }
+}
